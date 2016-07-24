d5a013e2c546a78db8ae28c68003b11c3f7b2753
muzanella <muzanella11@gmail.com>
2016-07-24 13:14:40 +0700

    docolist

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -12,9 +12,13 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css',
+        // 'css/site.css',
+        'themes/enem/media/css/app.min.css',
+        'https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css',
+        // 'https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css',
     ];
     public $js = [
+        'themes/enem/media/js/vendor.min.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
