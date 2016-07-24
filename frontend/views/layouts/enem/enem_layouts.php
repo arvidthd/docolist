79a8405c314fceb6e7efd420af95254214491ef3
muzanella <muzanella11@gmail.com>
2016-07-24 13:36:39 +0700

    docolist

---
+++ frontend/views/layouts/enem/enem_layouts.php
@@ -0,0 +1,43 @@
+<?php
+
+/* @var $this \yii\web\View */
+/* @var $content string */
+
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\bootstrap\Nav;
+use yii\bootstrap\NavBar;
+use yii\widgets\Breadcrumbs;
+use frontend\assets\AppAsset;
+use common\widgets\Alert;
+
+AppAsset::register($this);
+?>
+<?php $this->beginPage() ?>
+<!DOCTYPE html>
+<html class="no-js" lang="<?= Yii::$app->language ?>">
+<head>
+    <meta charset="<?= Yii::$app->charset ?>">
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <?= Html::csrfMetaTags() ?>
+    <title><?= Html::encode($this->title) ?> eneeemmm</title>
+    <?php $this->head() ?>
+</head>
+<body class="hold-transition skin-blue sidebar-mini">
+    <?php $this->beginBody() ?>
+        <div class="wrapper">
+            <?php
+                echo $this->render('//layouts/enem/partials/enem_header');
+            ?>
+            <?php
+                echo $content;
+            ?>
+            <?php
+                echo $this->render('//layouts/enem/partials/enem_footer');
+            ?>
+        </div>
+    <?php $this->endBody() ?>
+</body>
+</html>
+<?php $this->endPage() ?>
