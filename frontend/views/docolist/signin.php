93dfebdaa956ce0d50922a9a28c145bcdbb534bf
muzanella <muzanella11@gmail.com>
2016-07-24 13:35:23 +0700

    docolist

---
+++ frontend/views/docolist/signin.php
@@ -0,0 +1,25 @@
+<?php
+
+/* @var $this yii\web\View */
+/* @var $form yii\bootstrap\ActiveForm */
+/* @var $model \common\models\LoginForm */
+
+use yii\helpers\Html;
+use yii\bootstrap\ActiveForm;
+
+$this->title = 'Login';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="col-lg-12">
+    <div class="col-lg-4">
+        asd
+    </div>
+    <div class="col-lg-4">
+        asd
+    </div>
+    <div class="col-lg-4">
+        asd
+    </div>
+    <div class="clearfix"></div>
+</div>
+<div class="clearfix"></div>
