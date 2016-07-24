d5baebd79eceabb9bb0a0036487df463edfe9560
Muhammad Edi Sujarwo <muhammad.edi4@gmail.com>
2016-07-24 16:59:31 +0700

    edit conflict

--- backend/config/main-local.php
+++ backend/config/main-local.php
@@ -4,18 +4,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-<<<<<<< HEAD
-            'cookieValidationKey' => 'CUAI8AWVqQXSKIzZDA9-qOX7SxgR-V97',
-=======
-            'cookieValidationKey' => 'tdf_MseuKaXwxTEOPegKyoRnCeh4KOYy',
-        ],
-        'db' => [
-            'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=docolist',
-            'username' => 'root',
-            'password' => '',
-            'charset' => 'utf8',
->>>>>>> ddb2d2a07dc73f4869693f4b39c5eb74ce7ad2bf
+            'cookieValidationKey' => 'WfoNBF-8JUJJMmz1lMUtp_WaL_RpIv4f',
         ],
     ],
 ];
